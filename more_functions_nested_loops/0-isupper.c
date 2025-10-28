f20a2ce1017740c835fa25f3b0a8826c92a8a736
Niber236 <berniskouzou45@gmail.com>
2025-10-28 19:15:06 +0000

    red

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
-#include "main.h"
 /**
- * Your comment here
- */
-
+* _isupper - Vérifie si un caractère est une majuscule (A-Z)
+* @c: caractère à tester
+* Return: 1 si majuscule, 0 sinon
+*/
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-retour 1;
+return (1);
 else
-return 0;
+return (0);
 }
