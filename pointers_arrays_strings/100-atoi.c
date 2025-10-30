f959bf6f927c5100392ff34eeaf1fb06ed06ca98
Niber236 <berniskouzou45@gmail.com>
2025-10-30 23:59:30 +0000

    red

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,17 +9,23 @@ int _atoi(char *s)
 int i = 0;
 int sign = 1;
 int result = 0;
+int digit_found = 0;
 while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
 sign *= -1;
 else if (s[i] == '+')
+{
+}
 i++;
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
+digit_found = 1;
 result = result * 10 + (s[i] - '0');
 i++;
 }
+if (!digit_found)
+return (0);
 return (sign * result);
 }
