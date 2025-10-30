d0d0e667e39a87a584ba604ed11eb5e889d872b6
Niber236 <berniskouzou45@gmail.com>
2025-10-30 23:15:35 +0000

    red

---
+++ pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - écrit le caractère c sur stdout
+ * @c: le caractère à écrire
+ * Return: 1 si succès
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
