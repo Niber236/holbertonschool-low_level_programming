acb2de8c81648f8db38df25a075e91a7d28cd2a1
Niber236 <berniskouzou45@gmail.com>
2025-11-02 22:33:56 +0000

    red

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,29 @@
 #include "main.h"
-#include <string.h> /* pour strchr */
-#include <stddef.h> /* pour NULL */
 
 /**
-* cap_string - met une majuscule à chaque mot d'une chaîne
-* @str: la chaîne de caractères
-* Return: pointeur vers la chaîne modifiée
-*/
+ * cap_string - met une majuscule à chaque mot d'une chaîne
+ * @str: la chaîne de caractères
+ * Return: pointeur vers la chaîne modifiée
+ */
 char *cap_string(char *str)
 {
-int i = 0;
+int i = 0, j;
 char sep[] = " \t\n,;.!?\"(){}";
 
 while (str[i] != '\0')
 {
-if (i == 0 || strchr(sep, str[i - 1]) != NULL)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
+else
+{
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (str[i - 1] == sep[j] && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+break;
+}
+}
 }
 i++;
 }
