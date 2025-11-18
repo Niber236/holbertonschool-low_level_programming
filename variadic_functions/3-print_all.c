fa2cbcd884277b5eab19de25e60adc470ed04397
Niber236 <berniskouzou45@gmail.com>
2025-11-18 11:00:12 +0000

    rd

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,44 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
+
+/**
+ * _putchar - affiche un caractère (déjà fourni par Holberton)
+ * Prototype: int _putchar(char c);
+ */
+
+void print_number(int n)
+{
+    unsigned int num;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -n;
+    }
+    else
+        num = n;
+
+    if (num / 10)
+        print_number(num / 10);
+    _putchar((num % 10) + '0');
+}
+
+void print_float(double f)
+{
+    int int_part = (int)f;
+    double frac_part = f - int_part;
+    int i;
+
+    print_number(int_part);
+    _putchar('.');
+    if (frac_part < 0)
+        frac_part = -frac_part;
+    for (i = 0; i < 6; i++)
+    {
+        frac_part *= 10;
+        _putchar(((int)frac_part % 10) + '0');
+    }
+}
 
 /**
  * print_all - prints anything based on format
@@ -20,25 +58,36 @@ void print_all(const char * const format, ...)
         if (format[i] == 'c' || format[i] == 'i' ||
             format[i] == 'f' || format[i] == 's')
         {
+            if (sep[0])
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+
             if (format[i] == 'c')
-                printf("%s%c", sep, va_arg(ap, int));
+                _putchar(va_arg(ap, int));
             if (format[i] == 'i')
-                printf("%s%d", sep, va_arg(ap, int));
+                print_number(va_arg(ap, int));
             if (format[i] == 'f')
-                printf("%s%f", sep, (double)va_arg(ap, double));
+                print_float(va_arg(ap, double));
             if (format[i] == 's')
             {
                 s = va_arg(ap, char *);
                 if (!s)
-                    printf("%s(nil)", sep);
+                {
+                    _putchar('('); _putchar('n'); _putchar('i'); _putchar('l'); _putchar(')');
+                }
                 else
-                    printf("%s%s", sep, s);
+                {
+                    while (*s)
+                        _putchar(*s++);
+                }
             }
             sep = ", ";
         }
         i++;
     }
 
-    printf("\n");
+    _putchar('\n');
     va_end(ap);
 }
