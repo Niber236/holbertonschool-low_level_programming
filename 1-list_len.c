f4b91cf10c973ffaf2e5ae044d7257ea77fbf5ad
Niber91 <berniskouzou45@gmail.com>
2025-12-04 20:36:22 +0000

    0

---
+++ 1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to list
+*
+* Return: number of nodes
+*/
+size_t list_len(const list_t *h)
+{
+size_t count;
+
+count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
+}
